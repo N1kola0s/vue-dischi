1ca1d664993967bf744523e38e0e0da7eb1de3b9
N1kola0s <97747012+N1kola0s@users.noreply.github.com>
2022-05-05 23:00:02 +0200

    add MusicList component in MainComponent

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -5,33 +5,7 @@
         <section class="albums" v-if="!loading">
                 <div class="container h-100 p-5">
                     <div class="row row-cols-5 h-100">
-                            <div class="col g-3 h-100" v-for="(album, index) in albums" :key="index" >
-                                <div class="card bg_header p-3">
-                                    <img :src="album.poster" :alt="album.title" class="card-img-top">
-                                    <!-- card-img-top -->
-
-                                    <div class="card-body text-center">
-                                        <h5 class="card-title text-uppercase lh-0">{{album.title}}</h5>
-
-                                        <div class="card-text py-4">
-
-                                            <div class=album_title>
-                                                {{album.author}}
-                                            </div>
-                                            <!-- /.album_title -->
-                                            
-                                            <div class="album_year">
-                                                {{album.year}}
-                                            </div>
-                                            <!-- album_year -->
-                                        </div>
-                                        <!-- /.card-text -->                                     
-                                    </div>
-                                    <!-- /card-body -->
-                                </div>
-                                <!-- /.card -->
-                            </div>
-                            <!-- /.col -->
+                           <MusicList :album="album" v-for="(album, index) in albums" :key="index" />
                     </div>
                     <!-- /.row -->
                 </div>
@@ -50,12 +24,14 @@
 </template>
 
 <script>
-/* import MusicList from '@/components/MusicListComponent.vue'; */
-
+import MusicList from '@/components/MusicListComponent.vue';
 import axios from "axios"; 
 
     export default {
         name: 'MainComponent',
+        components: {
+            MusicList,
+        },
         data(){
             return{
                 url:'https://flynn.boolean.careers/exercises/api/array/music',
@@ -87,35 +63,6 @@ import axios from "axios";
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
     main{
-            height: calc(100% - 50px);
-
-                .card{
-                color: #959595;
-                max-height: 400px;
-                /* text-transform: uppercase; */
-
-                    .card-img-top{
-                        object-position: top;
-                        height: 200px;
-                        aspect-ratio: 1 / 1;
-                        object-fit: cover;
-
-                        
-                    }
-
-                    .card-body{
-                        padding: 1rem 0rem;
-
-                        .card-title{
-                            height: 10px;
-                            margin-bottom: 3rem;
-                        }
-
-                            h5{
-                            color:white;
-                        }
-                } 
-                
-            }
+            height: calc(100% - 50px);   
         }
 </style>
---
+++ src/components/MusicListComponent.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="col g-3 h-100" >
+        <div class="card bg_header p-3">
+            <img :src="album.poster" :alt="album.title" class="card-img-top">
+            <!-- card-img-top -->
+
+            <div class="card-body text-center">
+                <h5 class="card-title text-uppercase lh-0">{{album.title}}</h5>
+
+                <div class="card-text py-4">
+
+                    <div class=album_title>
+                        {{album.author}}
+                    </div>
+                    <!-- /.album_title -->
+                    
+                    <div class="album_year">
+                        {{album.year}}
+                    </div>
+                    <!-- album_year -->
+                </div>
+                <!-- /.card-text -->                                     
+            </div>
+            <!-- /card-body -->
+        </div>
+        <!-- /.card -->
+    </div>
+    <!-- /.col -->
+    
+</template>
+
+<script>
+
+export default {
+    name:'MusicList',
+    props:{
+        album: Object
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+
+    .card{
+        color: #959595;
+        max-height: 400px;
+        
+            .card-img-top{
+                object-position: top;
+                height: 200px;
+                aspect-ratio: 1 / 1;
+                object-fit: cover;
+            }
+
+            .card-body{
+                padding: 1rem 0rem;
+
+                .card-title{
+                    height: 10px;
+                    margin-bottom: 3rem;
+                }
+
+                    h5{
+                    color:white;
+                }
+        } 
+        
+    }
+
+</style>
