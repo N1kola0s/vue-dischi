db4315842751a4db53249835cb3d20d13b8e3a29
N1kola0s <97747012+N1kola0s@users.noreply.github.com>
2022-05-05 18:32:33 +0200

    add cards in main component

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,15 +1,13 @@
 <template>
-  <header id="site_header">
-    <nav class="bg_header d-flex align-items-center">
-      <div class="logo mx-3">
-        <img src="../assets/logo_spotify.png" alt="logo">
 
-      </div>
-      <!-- /.logo -->
-    </nav>
-
+  <header id="site_header" class=" bg_header d-flex align-items-center">
+    <div class="logo mx-3">
+      <img src="../assets/logo_spotify.png" alt="logo">
+    </div>
+    <!-- /.logo -->
   </header>
   <!-- /#site_header -->
+
 </template>
 
 <script>
@@ -23,14 +21,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-nav{
-  height: 50px;
-    .logo{
-      width: 36px;
-      aspect-ratio: 1 / 1;
-    }
-      
-}
-
-
+  header{
+      height: 50px;
+      .logo{
+        width: 36px;
+        aspect-ratio: 1 / 1;
+      }
+        
+  }
 </style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,6 +1,205 @@
 <template>
     <main id="site_main" class="bg_main">
 
+        <div class="container h-100 pt-5">
+
+            <div class="row row-cols-5">
+                <div class="col">
+
+                    <div class="card bg_header p-3">
+                        <img src="https://cdn.dribbble.com/users/2237555/screenshots/6860698/dribbble_base_g_4x.png?compress=1&resize=1000x750&vertical=top" alt="" class="card-img-top">
+                        <!-- card-img-top -->
+
+                        <div class="card-body text-center">
+                            <h5 class="card-title text-uppercase">Ciaone</h5>
+
+                            <p class="card-text py-4">Prova <br> 1998</p>
+                        </div>
+                        <!-- /.card-body -->
+
+                    </div>
+                    <!-- /.card -->
+
+                </div>
+                <!-- /.col -->
+
+                <div class="col">
+
+                    <div class="card bg_header p-3">
+                        <img src="https://cdn.dribbble.com/users/2237555/screenshots/6860698/dribbble_base_g_4x.png?compress=1&resize=1000x750&vertical=top" alt="" class="card-img-top">
+                        <!-- card-img-top -->
+
+                        <div class="card-body text-center">
+                            <h5 class="card-title text-uppercase">Ciaone</h5>
+
+                            <p class="card-text py-4">Prova <br> 1998</p>
+                        </div>
+                        <!-- /.card-body -->
+
+                    </div>
+                    <!-- /.card -->
+
+                </div>
+                <!-- /.col -->
+
+                <div class="col">
+
+                    <div class="card bg_header p-3">
+                        <img src="https://cdn.dribbble.com/users/2237555/screenshots/6860698/dribbble_base_g_4x.png?compress=1&resize=1000x750&vertical=top" alt="" class="card-img-top">
+                        <!-- card-img-top -->
+
+                        <div class="card-body text-center">
+                            <h5 class="card-title text-uppercase">Ciaone</h5>
+
+                            <p class="card-text py-4">Prova <br> 1998</p>
+                        </div>
+                        <!-- /.card-body -->
+
+                    </div>
+                    <!-- /.card -->
+
+                </div>
+                <!-- /.col -->
+
+                <div class="col">
+
+                    <div class="card bg_header p-3">
+                        <img src="https://cdn.dribbble.com/users/2237555/screenshots/6860698/dribbble_base_g_4x.png?compress=1&resize=1000x750&vertical=top" alt="" class="card-img-top">
+                        <!-- card-img-top -->
+
+                        <div class="card-body text-center">
+                            <h5 class="card-title text-uppercase">Ciaone</h5>
+
+                            <p class="card-text py-4">Prova <br> 1998</p>
+                        </div>
+                        <!-- /.card-body -->
+
+                    </div>
+                    <!-- /.card -->
+
+                </div>
+                <!-- /.col -->
+
+                <div class="col">
+
+                    <div class="card bg_header p-3">
+                        <img src="https://cdn.dribbble.com/users/2237555/screenshots/6860698/dribbble_base_g_4x.png?compress=1&resize=1000x750&vertical=top" alt="" class="card-img-top">
+                        <!-- card-img-top -->
+
+                        <div class="card-body text-center">
+                            <h5 class="card-title text-uppercase">Ciaone</h5>
+
+                            <p class="card-text py-4">Prova <br> 1998</p>
+                        </div>
+                        <!-- /.card-body -->
+
+                    </div>
+                    <!-- /.card -->
+
+                </div>
+                <!-- /.col -->
+
+                <div class="col">
+
+                    <div class="card bg_header p-3">
+                        <img src="https://cdn.dribbble.com/users/2237555/screenshots/6860698/dribbble_base_g_4x.png?compress=1&resize=1000x750&vertical=top" alt="" class="card-img-top">
+                        <!-- card-img-top -->
+
+                        <div class="card-body text-center">
+                            <h5 class="card-title text-uppercase">Ciaone</h5>
+
+                            <p class="card-text py-4">Prova <br> 1998</p>
+                        </div>
+                        <!-- /.card-body -->
+
+                    </div>
+                    <!-- /.card -->
+
+                </div>
+                <!-- /.col -->
+
+                <div class="col">
+
+                    <div class="card bg_header p-3">
+                        <img src="https://cdn.dribbble.com/users/2237555/screenshots/6860698/dribbble_base_g_4x.png?compress=1&resize=1000x750&vertical=top" alt="" class="card-img-top">
+                        <!-- card-img-top -->
+
+                        <div class="card-body text-center">
+                            <h5 class="card-title text-uppercase">Ciaone</h5>
+
+                            <p class="card-text py-4">Prova <br> 1998</p>
+                        </div>
+                        <!-- /.card-body -->
+
+                    </div>
+                    <!-- /.card -->
+
+                </div>
+                <!-- /.col -->
+
+                <div class="col">
+
+                    <div class="card bg_header p-3">
+                        <img src="https://cdn.dribbble.com/users/2237555/screenshots/6860698/dribbble_base_g_4x.png?compress=1&resize=1000x750&vertical=top" alt="" class="card-img-top">
+                        <!-- card-img-top -->
+
+                        <div class="card-body text-center">
+                            <h5 class="card-title text-uppercase">Ciaone</h5>
+
+                            <p class="card-text py-4">Prova <br> 1998</p>
+                        </div>
+                        <!-- /.card-body -->
+
+                    </div>
+                    <!-- /.card -->
+
+                </div>
+                <!-- /.col -->
+
+                <div class="col">
+
+                    <div class="card bg_header p-3">
+                        <img src="https://cdn.dribbble.com/users/2237555/screenshots/6860698/dribbble_base_g_4x.png?compress=1&resize=1000x750&vertical=top" alt="" class="card-img-top">
+                        <!-- card-img-top -->
+
+                        <div class="card-body text-center">
+                            <h5 class="card-title text-uppercase">Ciaone</h5>
+
+                            <p class="card-text py-4">Prova <br> 1998</p>
+                        </div>
+                        <!-- /.card-body -->
+
+                    </div>
+                    <!-- /.card -->
+
+                </div>
+                <!-- /.col -->
+
+                <div class="col">
+
+                    <div class="card bg_header p-3">
+                        <img src="https://cdn.dribbble.com/users/2237555/screenshots/6860698/dribbble_base_g_4x.png?compress=1&resize=1000x750&vertical=top" alt="" class="card-img-top">
+                        <!-- card-img-top -->
+
+                        <div class="card-body text-center">
+                            <h5 class="card-title text-uppercase">Ciaone</h5>
+
+                            <p class="card-text py-4">Prova <br> 1998</p>
+                        </div>
+                        <!-- /.card-body -->
+
+                    </div>
+                    <!-- /.card -->
+
+                </div>
+                <!-- /.col -->
+
+        </div>
+        <!-- /.row -->
+
+        </div>
+        
+
     </main>
 </template>
 
@@ -16,7 +215,28 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
     main{
-        height: calc(100vh - 50px);
-    }
+            height: calc(100% - 50px);
+                .card{
+                color: #959595;
+                /* text-transform: uppercase; */
 
+                    .card-img-top{
+                        object-position: top;
+                        height: 200px;
+                        aspect-ratio: 1 / 1;
+                        
+                        object-fit: cover;
+
+                        
+                    }
+
+                    .card-body{
+                        padding: 1rem 0rem;
+
+                            h5{
+                            color:white;
+                        }
+                }       
+            }
+        }
 </style>
